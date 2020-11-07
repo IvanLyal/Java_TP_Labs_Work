1230eba63f59f1f68ca0f0cc4a8b1dc9de5c95ed
TAJIKISTAN1 <parviz22022005@gmail.com>
2020-11-08 00:44:33 +0300

    Lab_Work_4

---
+++ Lab_4/Проект/Lab4_main.java
@@ -0,0 +1,12 @@
+public class Lab4_main {
+        public static void main(String[] args) {
+            programm1 app1 = new programm1();
+            app1.setVisible(true);
+            programm2 app2 = new programm2();
+            app2.setVisible(true);
+            programm3 app3 = new programm3();
+            app3.setVisible(true);
+            programm4 app4 = new programm4();
+            app4.setVisible(true);
+        }
+}
